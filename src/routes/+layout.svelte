894b052f15434d9a43c7bd09cc0111068377f4cc
NelsonDJCR <dev@nelsondjcr.com>
2024-02-15 07:23:02 -0500

    remove svelte

--- src/routes/+layout.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import './global.css';
-	import './tailwind.css';
-	import { setupViewTransition } from '$lib';
-</script>
-
-<slot />
--- src/routes/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import { setupViewTransition } from '$lib';
-	const { transition } = setupViewTransition();
-</script>
-
-<a href="/docs">sveltekit-view-transition</a>
-<h1><code use:transition={'title'} class="text-2xl">Hello</code></h1>
